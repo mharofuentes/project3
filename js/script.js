ddda0b7f13948b95d302ad140f85c35b93a39d8d
mharofuentes <90435653+mharofuentes@users.noreply.github.com>
2023-04-21 00:51:14 -0500

    script head of all four HTML files

--- js/script.js
+++ js/script.js
@@ -1,34 +1,26 @@
 function init(){
 	//alert('it works');
-	var el = document.getElementById('canvas');
-	var myLocation = new google.maps.LatLng(41.06118524999838, -90.93840600404329);
-	var mapOptions = {
-		center: myLocation,
-		zoom: 18,
-		mapTypeId: google.maps.MapTypeId.SATELLITE,
-		mapTypeControlOptions: {
-			position: google.maps.ControlPosition.BOTTOM_CENTER
-		}
-	};
-
-	var myMap = new google.maps.Map(el, mapOptions);
-
-	var marker = new google.maps.Marker({
-		position: myLocation,
-		map: myMap,
-		animation: google.maps.Animation.BOUNCE,
-	});
-
-
-	var infowindow = new google.maps.InfoWindow({
-      content: contentString
-  	});
-
-	google.maps.event.addListener(marker, 'mouseover', function() {
-    	infowindow.open(myMap, marker);
-  	});
-
-
+var el = document.getElementById('canvas');
+var myLocation = new google.maps.LatLng(41.06118524999838, -90.93840600404329);
+var mapOptions = {
+  center: myLocation,
+  zoom: 18,
+  mapTypeId: google.maps.MapTypeId.SATELLITE,
+  mapTypeControlOptions: {
+  position: google.maps.ControlPosition.BOTTOM_CENTER
 }
+};
+var myMap = new google.maps.Map(el, mapOptions);
 
+var marker = new google.maps.Marker({
+  position: myLocation,
+  map: myMap,
+  animation: google.maps.Animation.BOUNCE,
+});
+var infowindow = new google.maps.InfoWindow({
+ });
+  google.maps.event.addListener(marker, 'mouseover', function() {
+  infowindow.open(myMap, marker);
+});
+}
 google.maps.event.addDomListener(window, 'load', init);
