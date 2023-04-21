043893d32907e1e5aa3d2bab1df385c75f681b83
mharofuentes <90435653+mharofuentes@users.noreply.github.com>
2023-04-20 22:48:21 -0500

    changed map script

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
-function initMap() {
-  var mapOptions = {
-	zoom: 8,
-	center: new google.maps.LatLng(41.835080908035344, -87.62698444545988),
-	mapTypeId: 'satellite'
-};
+function init(){
+	//alert('it works');
+	var el = document.getElementById('canvas');
+	var myLocation = new google.maps.LatLng(41.06118524999838, -90.93840600404329);
+	var mapOptions = {
+		center: myLocation,
+		zoom: 18,
+		mapTypeId: google.maps.MapTypeId.SATELLITE,
+		mapTypeControlOptions: {
+			position: google.maps.ControlPosition.BOTTOM_CENTER
+		}
+	};
 
-var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+	var myMap = new google.maps.Map(el, mapOptions);
+
+	var marker = new google.maps.Marker({
+		position: myLocation,
+		map: myMap,
+		animation: google.maps.Animation.BOUNCE,
+	});
+
+	var contentString = '<h1>Big River State Forest</h1>;
+
+	var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  	});
+
+	google.maps.event.addListener(marker, 'mouseover', function() {
+    	infowindow.open(myMap, marker);
+  	});
+
+
 }
 
-
-
-
-
-
-
-
-
-
-
+google.maps.event.addDomListener(window, 'load', init);
